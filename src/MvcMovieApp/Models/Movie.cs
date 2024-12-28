9f84d8a512539116b2450ec618b80d5e89000619
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-12-28 17:19:01 +0530

    Swamy/27dec work (#3)
    * Update README.md
    
    * Initial Version
    
    * Changes
    
    * Update .gitignore
    
    * Code Formatting

--- src/MvcMovieApp/Models/Movie.cs
+++ src/MvcMovieApp/Models/Movie.cs
@@ -7,11 +7,11 @@ public class Movie
     public int Id { get; set; }
 
     public string? Title { get; set; }
-    
+
     [DataType(DataType.Date)]
     public DateTime ReleaseDate { get; set; }
-    
+
     public string? Genre { get; set; }
-    
+
     public decimal Price { get; set; }
 }
